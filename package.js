dadfbd65060958cfa910ac1144814d46d14501bd
Abigail Watson <awatson1978@gmail.com>
2016-11-30 00:11:12 -0600

    bump dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-imaging-study',
-  version: '1.1.0',
+  version: '1.2.0',
   summary: 'HL7 FHIR Resource - Imaging Study',
   git: 'https://github.com/clinical-meteor/hl7-resource-imaging-study',
   documentation: 'README.md'
@@ -17,9 +17,9 @@ Package.onUse(function (api) {
   api.use('prime8consulting:meteor-oauth2-server@0.0.2');
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.4.4');
-  api.use('clinical:hl7-resource-patient@1.3.0');
-  api.use('clinical:hl7-resource-practitioner@1.3.0');
+  api.use('clinical:hl7-resource-datatypes@0.5.6');
+  api.use('clinical:hl7-resource-patient@1.5.8');
+  api.use('clinical:hl7-resource-practitioner@1.5.1');
   api.use('clinical:hl7-resource-procedure@1.3.0');
   api.use('clinical:hl7-resource-diagnostic-order@1.5.0');
 
