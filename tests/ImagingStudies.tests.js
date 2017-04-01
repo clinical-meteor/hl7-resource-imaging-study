f116f7d1c0e7f6ab49461e96e544289b1a3aa048
Abigail Watson <awatson1978@gmail.com>
2017-04-01 00:32:05 -0500

    mocha tests

---
+++ tests/ImagingStudies.tests.js
@@ -0,0 +1,17 @@
+import { Meteor } from 'meteor/meteor';
+import { assert } from 'meteor/practicalmeteor:chai';
+import { chai } from 'meteor/practicalmeteor:chai';
+import { expect } from 'meteor/practicalmeteor:chai';
+
+describe('clinical:hl7-family-imaging-study', function () {
+  beforeEach(function () {
+    //console.log('beforeEach');
+  });
+  afterEach(function () {
+    //console.log('afterEach');
+  });
+  it('exists globally', function () {
+    expect(ImagingStudies).to.exist;
+  });
+});
+
