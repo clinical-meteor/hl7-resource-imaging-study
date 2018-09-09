19604161d3f8fd30da2a6b92cff6c7c411e74a01
Abigail Watson <abigailwatson@Abigails-Mac-mini-379644.local>
2018-09-09 01:57:52 -0500

    QA pass

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-imaging-study',
-  version: '1.4.0',
+  version: '1.4.1',
   summary: 'HL7 FHIR Resource - Imaging Study',
   git: 'https://github.com/clinical-meteor/hl7-resource-imaging-study',
   documentation: 'README.md'
